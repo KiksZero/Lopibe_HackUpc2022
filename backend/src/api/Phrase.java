f4a7bd15d119b2f118b646619a4ea4b1515477b3
DavidFalces <71983111+DavidFalces@users.noreply.github.com>
2022-04-30 01:52:32 +0200

    Add Phrase API v1

---
+++ backend/src/api/Phrase.java
@@ -0,0 +1,21 @@
+package api;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/phrase")
+public class Phrase {
+	//144.24.196.175/lopibes/services/phrase
+	@GET
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON}) 
+	@Path("/get")
+	public Response phrase() {
+		String result = "This is a test";
+		return Response.ok(result).build();
+	}
+}
