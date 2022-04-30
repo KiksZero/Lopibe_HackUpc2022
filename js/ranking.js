ee843b4bda7e925ea80ecb18fee2bb231b811fc9
erikblazquezupc <erik.blazquez@estudiantat.upc.edu>
2022-04-30 03:40:50 +0200

    cors

--- js/ranking.js
+++ js/ranking.js
@@ -7,7 +7,7 @@ function getRanking(params) {
         }
     };
     http.open('GET', 'http://144.24.196.175:8080/LoPibe/games'); // Método post y url invocada
-    http.setRequestHeader("Access-Control-Allow-Origin","*");
+    http.setRequestHeader("Access-Control-Allow-Origin","144.24.196.175:80");
     http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); // Establecer cabeceras de petición
     http.send(); // Envio de parámetros usando POST
 }
