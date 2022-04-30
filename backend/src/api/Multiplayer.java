78cdae692e9dbcd8c5894c6d6adafc45b0e3baf4
erikblazquezupc <erik.blazquez@estudiantat.upc.edu>
2022-04-30 23:42:08 +0200

    correcciones backend

--- backend/src/api/Multiplayer.java
+++ backend/src/api/Multiplayer.java
@@ -4,77 +4,111 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import domain.dataCtrl.DataCtrl;
 import domain.dataCtrl.DueloDataCtrl;
 
 @Path("/duelos")
 public class Multiplayer {
+
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON}) 
-	@Path("/create")
-	public Response create(@QueryParam("name1") String name1) {
-		DataCtrl dataCtrl = DataCtrl.getInstance();
-		DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
-		int result = ddc.insert(name1);
+	@Path("/")
+	public Response create(String content) {
+		int result = 0;
+		try {
+			JSONObject json = new JSONObject(content);
+			String name1 = json.getString("name1");
+			DataCtrl dataCtrl = DataCtrl.getInstance();
+			DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
+			result = ddc.insert(name1);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 		return Response.ok(result).header("Access-Control-Allow-Origin", "*").build();
 	}
 
-    @POST
+    @PUT
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON}) 
-	@Path("/start")
-	public Response start(@QueryParam("id") int id, @QueryParam("name2") String name2) {
-		DataCtrl dataCtrl = DataCtrl.getInstance();
-		DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
-		boolean result = ddc.updateName2(id, name2);
+	@Path("/{id}/start")
+	public Response start(@PathParam("id") int id, String content) {
+		boolean result = false;
+		try {
+			JSONObject json = new JSONObject(content);
+			String name2 = json.getString("name2");
+			DataCtrl dataCtrl = DataCtrl.getInstance();
+			DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
+			result = ddc.updateName2(id, name2);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 		return Response.ok(result).header("Access-Control-Allow-Origin", "*").build();
 	}
 
     @GET
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON}) 
-	@Path("/started")
-	public Response started(@QueryParam("id") int id) {
+	@Path("/{id}/started")
+	public Response started(@PathParam("id") int id) {
 		DataCtrl dataCtrl = DataCtrl.getInstance();
 		DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
 		boolean result = ddc.selectStarts(id);
 		return Response.ok(result).header("Access-Control-Allow-Origin", "*").build();
 	}
 
-    @POST
+    @PUT
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON}) 
-	@Path("/submitResult1")
-	public Response submitResult1(@QueryParam("id") int id, @QueryParam("r1") int r1) {
-		DataCtrl dataCtrl = DataCtrl.getInstance();
-		DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
-		boolean result = ddc.updateResult1(id, r1);
+	@Path("/{id}/submitResult1")
+	public Response submitResult1(@PathParam("id") int id, String content) {
+		boolean result = false;
+		try {
+			JSONObject json = new JSONObject(content);
+			int r1 = json.getInt("r1");
+			DataCtrl dataCtrl = DataCtrl.getInstance();
+			DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
+			result = ddc.updateResult1(id, r1);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 		return Response.ok(result).header("Access-Control-Allow-Origin", "*").build();
 	}
 
-    @POST
+    @PUT
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON}) 
-	@Path("/submitResult2")
-	public Response submitResult2(@QueryParam("id") int id, @QueryParam("r2") int r2) {
-		DataCtrl dataCtrl = DataCtrl.getInstance();
-		DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
-		boolean result = ddc.updateResult2(id, r2);
+	@Path("/{id}/submitResult2")
+	public Response submitResult2(@PathParam("id") int id, String content) {
+		boolean result = false;
+		try {
+			JSONObject json = new JSONObject(content);
+			int r2 = json.getInt("r2");
+			DataCtrl dataCtrl = DataCtrl.getInstance();
+			DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
+			result = ddc.updateResult2(id, r2);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 		return Response.ok(result).header("Access-Control-Allow-Origin", "*").build();
 	}
 
     @GET
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON}) 
-	@Path("/getResult1")
-	public Response getResults1(@QueryParam("id") int id) {
+	@Path("/{id}/getResult1")
+	public Response getResults1(@PathParam("id") int id) {
 		DataCtrl dataCtrl = DataCtrl.getInstance();
 		DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
 		int result = ddc.selectResult1(id);
@@ -84,8 +118,8 @@ public class Multiplayer {
     @GET
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON}) 
-	@Path("/getResult2")
-	public Response getResults2(@QueryParam("id") int id) {
+	@Path("/{id}/getResult2")
+	public Response getResults2(@PathParam("id") int id) {
 		DataCtrl dataCtrl = DataCtrl.getInstance();
 		DueloDataCtrl ddc = dataCtrl.getDueloDataCtrl();
 		int result = ddc.selectResult2(id);
