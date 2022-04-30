98015120eb40d9fd619b55d51883897320cd53fb
omar.tanveer.butt <omar.tanveer.butt@estudiantat.upc.edu>
2022-04-30 03:25:10 +0200

    fixed

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const WORDS = ['hola']
+const Phrase = "holaaaa"
 
 const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
-console.log(rightGuessString)
 
 function initBoard() {
-    let board = document.getElementById("game-board");
-
-    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+    	let board = document.getElementById("game-board");
         let row = document.createElement("div")
         row.className = "letter-row"
-        
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < Phrase.length; j++) {
             let box = document.createElement("div")
             box.className = "letter-box"
             row.appendChild(box)
-        }
+    		}
 
         board.appendChild(row)
-    }
 }
 
 initBoard()
