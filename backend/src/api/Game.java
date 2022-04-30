8a3c453b665a32d4bcd27d854f89997ce7445cb9
erikblazquezupc <erik.blazquez@estudiantat.upc.edu>
2022-04-30 05:17:40 +0200

    añadir resultado partida

--- backend/src/api/Game.java
+++ backend/src/api/Game.java
@@ -1,15 +1,19 @@
 package api;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import domain.controllers.TxAddGame;
 import domain.controllers.TxGetGames;
 
 @Path("/games")
@@ -31,5 +35,15 @@ public class Game {
 	@Path("/")
 	public Response options(){
 		return Response.ok().header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Headers", "*").header("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,DELETE,POST").build();
+	}
+
+	@POST
+	@Path("/")
+	public Response addGame(@QueryParam("playerName") String playerName, @QueryParam("score") int score, @QueryParam("scoreDate") Date scoreDate){
+		TxAddGame tx = new TxAddGame(playerName, score, scoreDate);
+		tx.execute();
+		boolean result = tx.getResult();
+		Response r = Response.ok(result).header("Access-Control-Allow-Origin", "*").build();
+		return r;
 	}
 }
