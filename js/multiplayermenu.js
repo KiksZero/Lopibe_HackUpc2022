8ed9af89685b2598b68d50deb289c441ad21cadb
DavidFalces <71983111+DavidFalces@users.noreply.github.com>
2022-05-01 05:43:43 +0200

    Improve communication in multiplayer menu

--- js/multiplayermenu.js
+++ js/multiplayermenu.js
@@ -1,29 +1,36 @@
 function createMatch(){
     var name = document.getElementById("name-input").value;
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var id = this.responseText;
-            window.location.href = "/multiplayer.html?name="+name+"&id="+id;
-        }
-    };
-    http.open('POST', 'http://144.24.196.175:8080/LoPibe/duelos');
-    http.setRequestHeader("Access-Control-Allow-Origin","*");
-    http.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-    http.send(JSON.stringify({'name1': name}));
+    if (name == "") alert('A name must be provided.');
+    else {
+        var http = new XMLHttpRequest();
+        http.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var id = this.responseText;
+                window.location.href = "/multiplayer.html?name="+name+"&id="+id;
+            }
+        };
+        http.open('POST', 'http://144.24.196.175:8080/LoPibe/duelos');
+        http.setRequestHeader("Access-Control-Allow-Origin","*");
+        http.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+        http.send(JSON.stringify({'name1': name}));
+    }
 }
 
 function enterMatch(){
     var id = document.getElementById("id-input").value;
     var name = document.getElementById("name-input").value;
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            window.location.href = "/multiplayer.html?name="+name+"&id="+id;
-        }
-    };
-    http.open('PUT', 'http://144.24.196.175:8080/LoPibe/duelos/start');
-    http.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-    http.setRequestHeader("Access-Control-Allow-Origin","*");
-    http.send(JSON.stringify({'name2': name, "id": id}));
+    if (name == "") alert('A name must be provided.');
+    else if (id == "") alert('A match code must be provided. If you do not have a match code, click "Create Match"');
+    else {
+        var http = new XMLHttpRequest();
+        http.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                window.location.href = "/multiplayer.html?name="+name+"&id="+id;
+            }
+        };
+        http.open('PUT', 'http://144.24.196.175:8080/LoPibe/duelos/start');
+        http.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+        http.setRequestHeader("Access-Control-Allow-Origin","*");
+        http.send(JSON.stringify({'name2': name, "id": id}));
+    }
 }
